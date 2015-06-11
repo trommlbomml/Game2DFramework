22a83d98596190b04787d37301abee1b8ad786cb
trommlbomml <peter_friedland@gmx.de>
2015-06-11 18:59:37 +0200

    1) Added Sprite class for Simpler change of Properties of a Sprite.
    2) Using MonoGame Icon.

---
+++ Game2DFramework/Drawing/Sprite.cs
@@ -0,0 +1,39 @@
+﻿
+using Game2DFramework.Extensions;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game2DFramework.Drawing
+{
+    public class Sprite
+    {
+        public Texture2D Texture { get; set; }
+        public Color Color { get; set; }
+        public Vector2 Position;
+        public Vector2 Scale;
+        public float Rotation;
+        public Rectangle ?SourceRectangle;
+        public Vector2 Origin;
+        public float Alpha;
+
+        public Sprite(Texture2D texture, Rectangle? sourceRectangle = null)
+        {
+            Texture = texture;
+            SourceRectangle = sourceRectangle;
+            Origin = Texture.GetCenter();
+            Color = Color.White;
+            Alpha = 1.0f;
+            Scale = new Vector2(1,1);
+        }
+
+        public void SetScale(float scale)
+        {
+            Scale = new Vector2(scale, scale);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, SourceRectangle, Color * Alpha, Rotation, Origin, Scale, SpriteEffects.None, 0);
+        }
+    }
+}
--- Game2dFramework.MonoGame/AnimationTest.cs
+++ Game2dFramework.MonoGame/AnimationTest.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Runtime.InteropServices;
+﻿using Game2DFramework.Drawing;
+using Game2DFramework.Extensions;
 using Game2DFramework.Interaction;
 using Game2DFramework.States;
 using Microsoft.Xna.Framework;
@@ -10,10 +10,7 @@ namespace Game2DFramework.MonoGame
     class AnimationTest : InitializableState
     {
         private Animator _iconAnimator;
-        private Texture2D _iconTexture2D;
-        private float _rotation;
-        private float _alpha;
-        private Vector2 _position;
+        private Sprite _iconSprite;
 
         protected override void OnEntered(object enterInformation)
         {
@@ -24,17 +21,18 @@ namespace Game2DFramework.MonoGame
         {
             _iconAnimator = new Animator();
             _iconAnimator.AddAnimation("BlendIn", new Animation(1.0f, AnimateBlendIn));
-
-            _iconTexture2D = Game.Content.Load<Texture2D>("smilie");
-            _rotation = 0.0f;
-            _alpha = 0.0f;
+            _iconSprite = new Sprite(Game.Content.Load<Texture2D>("MonoGameLogo"))
+            {
+                Position = new Vector2(Game.ScreenWidth*0.5f, Game.ScreenHeight*0.5f),
+                Alpha = 0.0f
+            };
         }
 
         private void AnimateBlendIn(float delta)
         {
-            _rotation = delta * delta * MathHelper.TwoPi;
-            _alpha = delta;
-            _position = new Vector2(Game.ScreenWidth * 0.25f, Game.ScreenHeight * 0.8f) + new Vector2(Game.ScreenWidth * 0.25f, -Game.ScreenHeight * 0.3f) * delta;
+            _iconSprite.Rotation = MathHelper.SmoothStep(0.0f, 1.0f, delta) * MathHelper.TwoPi;
+            _iconSprite.SetScale(MathHelper.SmoothStep(0.0f, 1.0f, delta));
+            _iconSprite.Alpha = delta;
         }
 
         public override void OnLeave()
@@ -51,7 +49,7 @@ namespace Game2DFramework.MonoGame
         public override void OnDraw(float elapsedTime)
         {
             Game.GraphicsDevice.Clear(Color.Black);
-            Game.SpriteBatch.Draw(_iconTexture2D, _position, null, Color.White * _alpha, _rotation, new Vector2(64, 64), 1.0f, SpriteEffects.None, 0);
+            _iconSprite.Draw(Game.SpriteBatch);
         }
     }
 }
